fc9efe293f6fb85a0917819eb3e71c391ebde272
Francesco Cassese <francescocassese9@gmail.com>
2026-07-23 13:52:07 +0200

    Initial commit: scaffold password generator form

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generatore di Password</title>
+</head>
+<body>
+    <header>
+        <h1>Generatore di Password</h1>
+    </header>
+    <main>
+    <form action="" method="get">
+
+    <label for="pass-length">Imposta lunghezza password</label>
+    <input type="number" id="pass-length" name="pass-length" placeholder="indica la lunghezza desiderata..." min="3" max="30">
+
+    <button type="submit">Genera la tua password</button>
+    </form>
+
+    </main>
+</body>
+</html>
